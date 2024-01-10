33a77d15643b78e78ba8237f3724b7463fa8fad3
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2024-01-10 13:40:01 +0545

    added mailing option including the form data

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import requests
+import smtplib
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+load_dotenv()
+
+my_email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
 url = "https://api.npoint.io/74f87b8a4eaa06fd0a4a"
 response = requests.get(url)
@@ -24,8 +32,25 @@ def contact():
         return render_template("contact.html")
     else:
         pass_item = "Successfully send your message"
-        return render_template("contact.html", item=pass_item)
+        name = request.form["username"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
 
+        # send email
+        with smtplib.SMTP("smtp.gmail.com") as connection:  # create an object from SMTP class
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="maheshchaulagain8@gmail.com",
+                                msg="Subject:Hello\n\n"
+                                    f"Name:{name}\n"
+                                    f"Email:{email}\n"
+                                    f"Phone:{phone}\n"
+                                    f"Message:{message}")
+        connection.close()
+
+        return render_template("contact.html", item=pass_item)
 
 
 @app.route('/post/<int:num>')
